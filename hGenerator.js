9eeb10a291626fd50c7f8db53217698d5ed0b00d
Martin Lehmann <martin@nith.no>
2014-01-10 19:45:57 +0100

    Rewrite the entire project. No more creepy ass JS "this"!

--- hGenerator.js
+++ hGenerator.js
@@ -1,13 +1,15 @@
+
 var line = '';
 for (var i = 0; i < 6; i++)
 {
-	line += "'";
+	line += "h";
+	line += (i + 1);
+	line += ": '";
+	
 	for (var j = 0; j <= i; j++)
 	{
 		line += '#';
 	}
-	line += "': 'h";
-	line += (i + 1);
 	line += "',";
 	line += "\n";
 }
--- mde.js
+++ mde.js
@@ -1,59 +1,59 @@
-/* Syntax */
-var containerTags =
-{
-	'#': 'h1',
-	'##': 'h2',
-	'###': 'h3',
-	'####': 'h4',
-	'#####': 'h5',
-	'######': 'h6',
-};
-
-$(document).ready(function()
-{
-
-	
-	$('#markdown').bind('input propertychange', function()
-	{
-		$this = $(this);
-
-		$html = $("#html");
-		var input = $this.val();
-
-		// Clear the output field.
-		$html.empty();
-
-		var lines = input.split('\n');
-
-		for (var i = 0; i < lines.length; i++)
-		{
-			var line = lines[i];
-
-			var output = -1; // nope
-
-			for (var key in a)
-			{
-				if (line.substring(0, key.length) === key)
-				{
-					var tag = a[key];
-					output = '<' + tag + '>' + line + '</' + tag + '>' 
-				}
-			}
-
-			if (output === -1)
-			{
-				output = line;
-			}
-
-			appendToHTML(output);
-		};
-
-		function appendToHTML(data)
-		{
-			$field = $('#html');
-			$existing = $field.html();
-
-			$field.html($existing + data);
-		}
-	});
+$(document).ready(function () {
+    mde.init()
 });
+
+var mde =
+{
+    banana: "mmmmmmm yellow",
+
+    init: function () {
+        $('#markdown').bind('input propertychange', this.parseAndUpdate)
+    },
+
+    parseAndUpdate: function () {
+        console.log("parseAndUpdate was called");
+
+        var $this = $('#markdown');
+
+        var $html = $("#html");
+        var input = $this.val();
+
+        // Clear the output field.
+        $html.empty();
+
+        var lines = input.split('\n');
+
+        for (var i = 0; i < lines.length; i++)
+        {
+            var line = lines[i];
+
+            var output = -1; // no match
+
+            // Check for headings.
+            for (var tag in tags.headings)
+            {
+                if (Object.prototype.hasOwnProperty.call(tags.headings, tag))
+                {
+                    if (line.substring(0, tags.headings[tag].length) === tags.headings[tag])
+                    {
+                        output = '<' + tag + '>' + line.substring(tags.headings[tag].length) + '</' + tag + '>';
+                    }
+                }
+            }
+
+            if (output === -1)
+            {
+                output = line;
+            }
+
+            appendToHTML(output)
+        }
+
+        function appendToHTML(data) {
+            $field = $('#html');
+            $existing = $field.html();
+
+            $field.html($existing + data);
+        }
+    }
+};
